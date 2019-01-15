ac9d4ada7fe5ab529d850186176506ba6512d30f
shaohuaxu <Yyxh1881>
2019-01-15 16:04:28 +0800

    集成joda类，用来专门处理日期类的操作

--- src/test/java/com/xu/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/xu/demo/SpringBootDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.xu.demo;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootDemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void jodaTest() {
+        DateTime dateTime = new DateTime();
+        System.out.println(dateTime.dayOfYear().get());
+    }
 
 }
