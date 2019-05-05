dd231d95d282bd6acc8b2547796f2334af73928a
shaohua <v-shaohuaxu@creditease.cn>
2019-05-05 14:26:55 +0800

    集成处理图片的工具类

--- src/test/java/com/xu/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/xu/demo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.xu.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SpringBootDemoApplicationTests {
 
     @Test
@@ -20,4 +25,19 @@ public class SpringBootDemoApplicationTests {
         System.out.println(dateTime.dayOfYear().get());
     }
 
+    @Test
+    public void imageTest() {
+        try {
+            //BufferedImage bufferedImage = Thumbnails.of("D:\\a.jpg").size(1280, 1024).asBufferedImage();
+            //ImageIO.write(bufferedImage, "jpg", new File("D:\\b.jpg"));
+
+            //Thumbnails.of("D:\\a.jpg").scale(0.25f).toFile("D:\\c.jpg");
+
+            Thumbnails.of("D:\\a.jpg").size(80, 80).keepAspectRatio(false).toFile("D:\\d.jpg");
+
+            log.info("图片处理成功！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
