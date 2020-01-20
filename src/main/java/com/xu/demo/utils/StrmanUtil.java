b2058deb61a3f590e451d479c2f5c6dc54d7d75c
shaohua <shaohua.xu@tendcloud.com>
2020-01-20 10:25:27 +0800

    引用Strman的字符串处理类

---
+++ src/main/java/com/xu/demo/utils/StrmanUtil.java
@@ -0,0 +1,18 @@
+package com.xu.demo.utils;
+
+import strman.Strman;
+
+/**
+ * Strman处理字符串
+ *
+ * @author: shaohua
+ * @date: 2020-01-19.
+ **/
+public class StrmanUtil {
+    public static void main(String[] args) {
+        //Base64加密/解密
+        //System.out.println(Strman.base64Encode("xushaohua"));
+        //System.out.println(Strman.binEncode("xushaohua"));
+        //Strman.split()
+    }
+}
